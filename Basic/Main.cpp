1c8db88ca4645628d34eab85395cf063d2abd11c
Himanshu kumar <himkumsharma@gmail.com>
2024-03-03 01:42:36 +0530

    Find Average number

---
+++ Basic/Main.cpp
@@ -0,0 +1,15 @@
+// find Average of three number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,Avg;
+    cout<< " Enter the value of a,b and c"<<endl;
+    cin>> a>>b>>c;
+    
+    Avg= (a+b+c)/3;
+    cout<<"Avg value "<<Avg<<endl;
+    
+
+
+}
