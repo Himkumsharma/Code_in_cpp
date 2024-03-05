5921b816c48c0dbd6d36b12952714933393af99e
Himanshu kumar <himkumsharma@gmail.com>
2024-03-05 17:48:45 +0530

    shwo even or odd number

---
+++ Basic/even_Odd_find_out.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the Number"<<endl;
+    cin>>n;
+            
+   if( n%2 == 0)
+   {
+    cout<<"Number is Even"<<endl;
+
+   }
+   else {
+    cout <<"Number is odd"<<endl;
+
+   }
+   return 0;
+}
